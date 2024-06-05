b55d05fa58319f6be1570198e260824276d2fdd2
rudi <neitzkerudilberto@gmail.com>
2024-06-05 14:22:02 -0300

    add funcao de baixar a nota apos emitir

--- Nota.py
+++ Nota.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+import pyperclip
 
 class Nota():
 
@@ -48,6 +49,15 @@ class Nota():
         volumes = pt.prompt(text='Digite quantos volumes')
         pt.alert(text='Não mexa no computador até a nota ser finalizada', title='alerta')
 
+        pt.PAUSE = 0.8
+        # Acha o programa
+        pt.hotkey('alt', 'tab')
+        time.sleep(1.5)
+
+        while not pt.locateOnScreen(f'{self.logo}', confidence=0.9, grayscale=True):
+            time.sleep(1)
+            pt.hotkey('alt', 'shift', 'tab')
+
         time.sleep(2)
         pt.PAUSE = 0.8
 
@@ -55,7 +65,13 @@ class Nota():
         pt.write('F140GNF')
         pt.press('enter')
 
-        pt.press('tab', presses=4, interval=0.1)
+        pt.press('tab')
+        pt.hotkey('ctrl', 'c')
+        time.sleep(0.5)
+        nota = pyperclip.paste()
+        print(nota)
+
+        pt.press('tab', presses=3, interval=0.1)
         pt.write('1107')
         pt.press('tab')
         pt.write('5911l')
@@ -90,3 +106,36 @@ class Nota():
         pt.hotkey('alt', 's')
         pt.hotkey('alt', 'o')
         pt.hotkey('alt', 'o')
+
+        pt.hotkey('ctrl', 'f4')
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless=new')
+        service = Service(ChromeDriverManager().install())
+
+        navegador = webdriver.Chrome(options=options, service=service)
+
+        navegador.get(r'http://192.168.0.13/SDE/Login.aspx?ReturnUrl=%2fsde%2f')
+
+        navegador.find_element('xpath', '//*[@id="txbLogin"]').send_keys('nfe')
+        navegador.find_element('xpath', '//*[@id="txbSenha"]').send_keys('nfe')
+        time.sleep(1)
+        navegador.find_element('xpath', '//*[@id="btnLogin"]').click()
+
+        navegador.find_element('xpath', '//*[@id="acdMenu"]/div[3]').click()
+        time.sleep(1)
+        navegador.find_element('xpath', '//*[@id="ctl13_lkbNfeEmissao"]').click()
+        time.sleep(1)
+        navegador.find_element('xpath', '//*[@id="btnFiltrarNfe"]').click()
+        time.sleep(1)
+
+        navegador.find_element('xpath',
+                                '//*[@id="filtrofltEmissao3"]').send_keys(nota, Keys.ENTER)
+        time.sleep(1)
+
+        navegador.find_element('xpath', '//*[@id="nfeGridInfo"]').click()
+        navegador.find_element('xpath', '//*[@id="btnDanfe"]').click()
+
+        time.sleep(2)
+
+        navegador.quit()
